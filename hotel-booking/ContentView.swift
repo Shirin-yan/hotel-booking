2d3d31c591e516a847296adf4f43308f5c946195
Shirin <shirin@Shirins-Laptop.local>
2023-12-20 11:32:05 +0500

    created project

---
+++ hotel-booking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hotel-booking
+//
+//  Created by Shirin on 20.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hotel-booking/hotel_bookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotel_bookingApp.swift
+//  hotel-booking
+//
+//  Created by Shirin on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct hotel_bookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
