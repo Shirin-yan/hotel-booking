557a8349475c3f33e2a85dab5923a3448f6e1af0
Shirin <shirin@Shirins-Laptop.local>
2023-12-23 10:42:41 +0500

    Payment done

--- hotel-booking/View/PaymentView.swift
+++ hotel-booking/View/PaymentView.swift
@@ -11,7 +11,38 @@ struct PaymentView: View {
     @EnvironmentObject var coordinator: Coordinator
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Image("congrats")
+                .padding(25)
+                .background(Color.bgGray)
+                .cornerRadius(50)
+                .padding(.bottom, 12)
+            
+            Text("Ваш заказ принят в работу")
+                .foregroundColor(.black)
+                .font(.med_22)
+                .multilineTextAlignment(.center)
+            
+            Text("Подтверждение заказа №104893 может занять некоторое время (от 1 часа до суток). Как только мы получим ответ от туроператора, вам на почту придет уведомление.")
+                .foregroundColor(.customLightGray)
+                .font(.reg_16)
+                .multilineTextAlignment(.center)
+        }.padding(20)
+            .navigationTitle("Заказ оплачен")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Text("Супер!")
+                        .frame(maxWidth: .infinity, minHeight: 48)
+                        .foregroundColor(.white)
+                        .font(.med_16)
+                        .background(Color.customBlue)
+                        .cornerRadius(15)
+                        .padding(.top, 12)
+                        .onTapGesture {
+                            coordinator.popToRoot()
+                        }
+                }
+            }
     }
 }
 
